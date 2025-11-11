c15c51db878a87819e50679816194f46fe7a3bc9
Will McLean <wmclean@uvm.edu>
2025-11-11 16:38:28 -0500

    fix

--- TrailMaps/reviewcode/append.php
+++ TrailMaps/reviewcode/append.php
@@ -1,8 +1,10 @@
 <?php
+
+$name = "name";
 // Open the CSV file in append mode
 $csv = fopen("review.csv", "a");
 // Define the row to be appended
-$row = ["Test", "var"];
+$row = [$name, "var"];
 // Append the row to the CSV file
 fputcsv($csv, $row, escape: "");
 // Close the CSV file
