214d783ad8b90a28c9935c9a8ec88d7e92633f5d
Will McLean <wmclean@uvm.edu>
2025-11-11 16:00:42 -0500

    append

--- append.php
+++ append.php
@@ -2,7 +2,7 @@
 // Open the CSV file in append mode
 $csv = fopen("review.csv", "a");
 // Define the row to be appended
-$row = ["A", "B"];
+$row = ["Test", "var"];
 // Append the row to the CSV file
 fputcsv($csv, $row, escape: "");
 // Close the CSV file
