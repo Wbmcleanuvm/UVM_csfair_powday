a17adcd862a3903ba6b22a53477617622b109928
Will McLean <wmclean@uvm.edu>
2025-11-13 13:58:26 -0500

    added smuggstrails and updated mountain

--- Mountain.js
+++ Mountain.js
@@ -1,10 +1,45 @@
 class Mountain 
 {
-    // Use this class for blueprint for each mountain data
-    
+    #mountName;
+    #location;
+    #trails;
 
+    Mountain(mountName, location, trails){
+        this.mountName = mountName;
+        this.location = location
+        this.trails = trails
+    
+    }
     readDataFromSite() 
     {
     //  Parse through website data   
+    }
+    numberOfTrails() 
+    {
+    //  Return number of trails on mountain
+    }
+    numberOfTrailsOpen() 
+    {
+
+    }
+}
+class trail extends Mountain
+{
+    #name;
+    #difficulty;
+    #status;
+    trail(name, difficulty,status){
+        this.#name = name;
+        this.#difficulty = difficulty;
+        this.#status = status;
+    }
+    getName(){
+        return this.#name;
+    }
+    getDifficulty(){
+        return this.#difficulty;
+    }
+    getStatus(){
+        return this.#status;
     }
 }
