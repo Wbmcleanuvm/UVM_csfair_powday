78814d37de14b1aec93c7d52c464228ad089dfb8
Will McLean <wmclean@uvm.edu>
2025-11-26 21:27:47 -0500

    fixing read

--- Mountain.js
+++ Mountain.js
@@ -43,6 +43,39 @@ class Mountain
    
 
 }
+function convertToTrails(trailData){
+    let add = "";
+    let trails = [];
+    let tconst = [];
+    let count = 0;
+    let i = 0;
+    while ( i < trailData.length){
+        if (trailData[i] == ","){  
+            count++;
+            i++;
+            tconst.push(add);
+            add = "";
+        }
+        else if (trailData[i] == "["){
+            i++;
+            count = 0;
+        }
+        else if (trailData[i] == "]"){
+            newTrail = new trail(tconst[0], tconst[1], tconst[2]);
+            trails.push(newTrail);
+            i++;
+            tconst = [];
+        }
+        add += trailData[i];
+        console.log(add);
+        i++;
+
+    }
+    console.log(trails);
+
+
+
+}
 class trail extends Mountain
 {
     #name;
