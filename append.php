d263ecd4d3622bbc105fe0c9b85c914f9833e25b
Will McLean <wmclean@uvm.edu>
2025-11-28 22:59:02 -0500

    Added Trail review option for Review

--- append.php
+++ append.php
@@ -3,11 +3,12 @@
 $name = $_POST['fname'];
 $rating = $_POST['score'];
 $mnt = $filename = $_POST['mnt'];
+$trail = $_POST['Trail'];
 
 // Open the CSV file in append mode
 $csv = fopen("{$filename}review.csv", "a");
 // Define the row to be appended
-$row = [$name, $rating, $mnt];
+$row = [$name, $rating, $mnt, $trail];
 // Append the row to the CSV file
 fputcsv($csv, $row, escape: "");
 // Close the CSV file
